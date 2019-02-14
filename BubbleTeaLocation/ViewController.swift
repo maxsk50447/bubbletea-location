9c518d6233a4a075e335e0adeb9c4a7f8296de88
Patipol Wangjaitham <patipol.wa@ku.th>
2019-02-14 13:29:35 +0700

    Initial Commit

---
+++ BubbleTeaLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BubbleTeaLocation
+//
+//  Created by Patipol Wangjaitham on 14/2/2562 BE.
+//  Copyright © 2562 Patipol Wangjaitham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
